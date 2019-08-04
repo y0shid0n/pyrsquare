1d0788475879f2cb0197a2bb77df1f185872c300
NaohiroYoshida <y0shid0n1031@gmail.com>
2019-08-04 13:51:26 +0900

    単位カラム処理の条件修正

--- lib/myfunc.py
+++ lib/myfunc.py
@@ -106,7 +106,7 @@ def list_to_pd(result_list):
             result_list[0].insert(3, result_list[0][1] + "_unit")
             result_list[0].append(result_list[0][-1] + "_unit")
             result_list[0].insert(1, "blank1")
-            result_list[0].insert(4, "blank2")
+            result_list[0].insert(5, "blank2")
             result_df = pd.DataFrame(result_list[1:], columns=result_list[0])
         elif max(col_num_list) - len(result_list[0]) == 2:
             result_list[0].insert(-2, result_list[0][-3] + "_unit")
