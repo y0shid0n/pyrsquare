447248108a9a91f80917e6917bdd79c1aa5eca15
NaohiroYoshida <y0shid0n1031@gmail.com>
2019-08-05 23:14:45 +0900

    空白文字の削除を追加

--- parse_xbrl.py
+++ parse_xbrl.py
@@ -117,6 +117,9 @@ for file in file_list:
     df_output["fs_type"] = fs_type
     df_output["acc_standard"] = acc_standard
 
+    # 全カラムの前後にある空白文字を削除
+    df_output = df_output.applymap(lambda x: x.strip())
+
     output_filename = "./output/parsed_csv/{}_{}_test.csv".format(ecode, fs_type)
     df_output.to_csv(output_filename, sep=",", index=False, encoding="utf-8")
 
